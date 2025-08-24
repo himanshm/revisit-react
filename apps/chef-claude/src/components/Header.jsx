087cc223cb2f0a3aea896f6c2cd8aac2f2876b13
Himanshu Mishra <himanshu.mishra@aerchain.io>
2025-08-25 00:23:15 +0530

    add static stuff of the page

---
+++ apps/chef-claude/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import chefClaudeImg from '../assets/chef-claude.png'
+
+function Header() {
+  return (
+    <header className='header'>
+        <img src={chefClaudeImg} alt='chef-claude logo' className='logo-img' />
+        <h1>Chef Claude</h1>
+    </header>
+  )
+}
+
+export default Header
