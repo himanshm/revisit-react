ee318ca05cba08565175ec357646dca88b1a8dad
Himanshu Mishra <himanshu.mishra@aerchain.io>
2025-09-18 13:08:42 +0530

    chore: add state for dice values
    add roll button with styles

--- apps/tenzies/src/App.jsx
+++ apps/tenzies/src/App.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import Die from './components/Die';
+
 const App = () => {
-  const dice = Array.from({ length: 10 }, () => ({
-    id: crypto.randomUUID(),
-    value: Math.ceil(Math.random() * 6)
-  }));
+  const generateAllNewDice = () =>
+    Array.from({ length: 10 }, () => ({
+      // id: crypto.randomUUID(),
+      id: Math.random().toString(36).slice(2, 8), // Other way to generate random ids
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    }));
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  const handleDiceRoll = () => {
+    setDice(generateAllNewDice());
+  };
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />);
 
   return (
-    <main>
-      <div className="dice-container">
-        {dice.map(die => (
-          <Die key={die.id} value={die.value} />
-        ))}
-      </div>
-    </main>
+    <>
+      <main>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice-btn" onClick={handleDiceRoll}>
+          Roll
+        </button>
+      </main>
+    </>
   );
 };
 
